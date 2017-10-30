d233197f64c8ad2d1a394f808b99a5b11df3fa5b
Atom0407 <tenten0407team@yahoo.co.jp>
2017-10-30 13:14:12 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'salty-shelf-44867.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'secret-springs-32540.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
-   user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_PASSWORD'],
+   user_name: ENV['app79631946@heroku.com'],
+   password: ENV['kglyrqeb7974'],
    domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
