7b0edeab0dc4cb93dbd329e1130999a46a7fbf3f
Atom0407 <tenten0407team@yahoo.co.jp>
2017-11-04 18:59:38 +0000

    1105

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -8,14 +8,14 @@ class NoticeMailer < ApplicationMailer
   def sendmail_blog(blog)
     @blog = blog
 
-    mail to: "tenten0407team@yahoo.co.jp",
+    mail to: tenten0407team@yahoo.co.jp,
       subject: '【Achieve】ブログが投稿されました'
   end
 
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to:@contact.email,
+    mail to: @contact.email,
       subject: '【Achieve】に問い合わせが完了しました！'
   end
 end
